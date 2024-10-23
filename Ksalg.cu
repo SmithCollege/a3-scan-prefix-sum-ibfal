902a2adea32e69dbcf937b2c287f131720fa6e4d
bfalbo <bfalbo@smith.edu>
2024-10-23 15:28:24 -0400

    new Kogge-stone

--- Ksalg.cu
+++ Ksalg.cu
@@ -1,50 +1,54 @@
 #include <iostream>
 #include <math.h>
 
-#define SIZE 1024
+#define SIZE 8
 
 //kernel funciton for prefix wihtmultiple threads
 __global__ 
-void prefixsum(int *in, int *out){
-		__shared__ int dest[SIZE];
+void prefixsum(int *in, int *out, int stride){
 		int xindex = threadIdx.x;
-        int index = blockIdx.x*blockDim.x+threadIdx.x;
+        //int index = blockIdx.x*blockDim.x+threadIdx.x;
         
-		if (index< SIZE){
-			dest[xindex]= in[index];
-		}
-
-        for (int stride = 1; stride< blockDim.x; stride++){
-        	__syncthreads();
-			int val; 
-        	if (xindex >= stride){
-  				val= in[xindex - stride];} //dest[xindex]= in[xindex]+ in[xindex - stride]
-			__syncthreads();
-			if (xindex >=stride){
-				dest[xindex]+= val;}		
+        if (xindex >= stride){
+  			out[xindex] = in[xindex]+ in[xindex - stride];
+  			}		
+  			
+   		if (xindex < stride){
+        	out[xindex]= in[xindex];}
         }
-        out[index]= dest[xindex];
-        }
-
 
 int main(void){
-        int * input, * output;
+        int *input, *output, *temp, *source, *dest;
         cudaMallocManaged(&input, sizeof(int)*SIZE);
         cudaMallocManaged(&output, sizeof(int)*SIZE);
+        //cudaMallocManaged(&temp, sizeof(int)*SIZE);
+        //cudaMallocManaged(&dest, sizeof(int)*SIZE);
+        //cudaMallocManaged(&source, sizeof(int)*SIZE);
 
         //init input array
-        for (int i=0; i<SIZE; i++){
-                input[i]= 1;
-        }
+		input[0]= 3;
+		input[1]= 1;
+		input[2]= 7;
+		input[3]= 0;
+		input[4]= 4;
+		input[5]= 1;
+		input[6]= 6;
+		input[7]= 3;
 
+		source = &input[0];
+		dest = &output[0];
+		
         for (int i = 0; i<SIZE; i++){
         		printf("%d ", input[i]);}
         printf("\n");
         
         // call prefixsum
-        
-        prefixsum<<<1,SIZE>>>(input,output);//doesnt work for multiple blocks or more than 1024 threads. 
-
+        for (int stride = 1; stride < SIZE; stride*=2){
+        	prefixsum<<<1,SIZE>>>(source, dest, stride);
+        	temp = dest; 
+        	output = source;
+        	source = temp ;
+		}
 	
         //sync
         cudaDeviceSynchronize();
